577dba239650094911ab31ed9d43b6c0d08f1313
Kim Hyeonseok <kim010322@gmail.com>
2022-06-18 00:31:46 +0900

    GCP 크론 디버깅

--- src/logger.ts
+++ src/logger.ts
@@ -5,9 +5,8 @@ const logger = createLogger({
     transports: [new transports.Console()],
     format: format.combine(
         format.colorize(),
-        format.timestamp(),
         format.printf(({ timestamp, level, message }) => {
-            return `[${timestamp}] ${level}: ${message}`;
+            return `[${level}: ${message}`;
         })
     ),
 })
