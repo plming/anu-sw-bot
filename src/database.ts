11ad45f770050ffc217531ebb349eaf90f5840d1
Kim Hyeonseok <kim010322@gmail.com>
2022-07-07 17:18:29 +0900

    비동기 처리 리팩토링

--- src/database.ts
+++ src/database.ts
@@ -1,60 +1,47 @@
 import assert from "node:assert";
-import { Collection, MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";
 import "dotenv/config";
 
 import { Business, IBusiness } from "./business";
 import { Notice, INotice } from "./notice";
 
-let businesses: Collection<IBusiness>;
-let notices: Collection<INotice>;
+assert(process.env.MONGODB_URI !== undefined);
 
-(() => {
-    assert(process.env.MONGODB_URI !== undefined);
+const client = new MongoClient(process.env.MONGODB_URI);
+const database = client.db('AnuSw');
 
-    const client = new MongoClient(process.env.MONGODB_URI);
-    const database = client.db('AnuSw');
-
-    businesses = database.collection('business');
-    notices = database.collection('notice');
-})();
+let businesses = database.collection<IBusiness>('business');
+let notices = database.collection<INotice>('notice');
 
 export async function findBusiness(id: number): Promise<Business | null> {
     let found = await businesses.findOne({ _id: id });
-    if (found === null) {
-        return null;
-    } else {
-        return new Business(
-            found._id,
-            found.title,
-            found.department,
-            found.bodyText,
-            found.applicationStartDate,
-            found.applicationEndDate
-        )
-    }
-};
+    return found === null ? null : new Business(
+        found._id,
+        found.title,
+        found.department,
+        found.bodyText,
+        found.applicationStartDate,
+        found.applicationEndDate
+    )
+}
 
 export async function insertBusiness(business: Business): Promise<void> {
     let result = await businesses.insertOne(business);
     assert(result.acknowledged);
-};
+}
 
 export async function findNotice(id: number): Promise<Notice | null> {
     let found = await notices.findOne({ _id: id });
-    if (found === null) {
-        return null;
-    } else {
-        return new Notice(
-            found._id,
-            found.title,
-            found.bodyText,
-            found.author,
-            found.createdAt
-        )
-    }
+    return found === null ? null : new Notice(
+        found._id,
+        found.title,
+        found.bodyText,
+        found.author,
+        found.createdAt
+    )
 }
 
 export async function insertNotice(notice: Notice): Promise<void> {
     let result = await notices.insertOne(notice);
     assert(result.acknowledged);
-};
+}
--- src/main.ts
+++ src/main.ts
@@ -9,36 +9,48 @@ import logger from './logger';
 const app = express();
 const PORT = process.env.PORT ?? 8080;
 
-app.get('/run', async (_req, res) => {
+app.listen(PORT, () => {
+    logger.info(`서버가 ${PORT}번 포트에서 listen중입니다...`);
+});
+
+app.get('/run', (_req, res) => {
+    Promise.all([handleBusiness(), handleNotice()])
+        .then(() => {
+            res.status(200).send('게시판 크롤링, 슬랙방 공지를 완료했습니다');
+        }).catch(() => {
+            res.status(500).send('작업이 실패했습니다');
+        })
+        .finally(() => {
+            logger.info("hello");
+        })
+});
+
+async function handleBusiness() {
     const businesses = await board.getBusinesses();
     for (const business of businesses) {
         let found = await db.findBusiness(business._id);
 
         if (found === null) {
-            slack.notifyBusinessAdded(business);
-            db.insertBusiness(business);
+            await slack.notifyBusinessAdded(business);
+            await db.insertBusiness(business);
         }
         else {
             logger.info(`이미 알림한 지원사업입니다. ${business.title}`);
         }
     }
+}
 
+async function handleNotice() {
     const notices = await board.getNotices();
     for (const notice of notices) {
         let found = await db.findNotice(notice._id);
 
         if (found === null) {
-            slack.notifyNoticeAdded(notice);
-            db.insertNotice(notice);
+            await slack.notifyNoticeAdded(notice);
+            await db.insertNotice(notice);
         }
         else {
             logger.info(`이미 알림한 공지사항입니다. ${notice.title}`);
         }
     }
-
-    res.status(200).send('게시판 크롤링 및 슬랙방 공지를 완료했습니다');
-});
-
-app.listen(PORT, () => {
-    logger.info(`서버가 ${PORT}번 포트에서 listen중입니다...`);
-})
+}
