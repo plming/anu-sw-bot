f68d34aa34444b15f830f73efe52e23735f5dfde
Kim Hyeonseok <kim010322@gmail.com>
2022-07-14 00:58:17 +0900

    refactoring

--- src/board.ts
+++ src/board.ts
@@ -6,13 +6,13 @@ import { Business } from "./business";
 import { Notice } from './notice';
 
 export async function getCurrentBusinessIds(): Promise<number[]> {
-    const parentPage = await loadDocument("https://sw.anu.ac.kr/main/sw/jw/main/list.php", {
+    const $ = await loadDocument("https://sw.anu.ac.kr/main/sw/jw/main/list.php", {
         "search_bzstat": "S" // 접수중 상태인 지원사업만 조회 
     });
 
     const ids: number[] = [];
-    const nodeList = parentPage(".lc_title_M")
-    for (const node of nodeList) {
+    const nodes = $(".lc_title_M")
+    for (const node of nodes) {
         // 예를 들어 "javascript:goView('397')"; 에서 397을 추출함
         const onClickText = node.attribs['onclick'];
         const splited = onClickText.split("\'");
@@ -27,11 +27,11 @@ export async function getCurrentBusinessIds(): Promise<number[]> {
 }
 
 export async function getBusiness(id: number): Promise<Business> {
-    const childPage = await loadDocument("https://sw.anu.ac.kr/main/sw/jw/main/view.php", { "bznum": id });
-    const title = childPage('.th1:contains("사업명")').next().text().trim();
-    const department = childPage('.th1:contains("담당부서")').next().text().trim();
-    const bodyText = childPage('.bbs_content').text().trim();
-    const [start, end] = childPage('.th1:contains("신청기간")').next().text().split(' ~ ');
+    const $ = await loadDocument("https://sw.anu.ac.kr/main/sw/jw/main/view.php", { "bznum": id });
+    const title = $('.th1:contains("사업명")').next().text().trim();
+    const department = $('.th1:contains("담당부서")').next().text().trim();
+    const bodyText = $('.bbs_content').text().trim();
+    const [start, end] = $('.th1:contains("신청기간")').next().text().split(' ~ ');
     const applicationStartDate = new Date(start);
     const applicationEndDate = new Date(end);
 
@@ -39,12 +39,11 @@ export async function getBusiness(id: number): Promise<Business> {
 }
 
 export async function getCurrentNoticeIds(): Promise<number[]> {
-    const parentPage = await loadDocument('https://sw.anu.ac.kr/module/bbs/list.php', { 'mid': '/community/notice' });
+    const $ = await loadDocument('https://sw.anu.ac.kr/module/bbs/list.php', { 'mid': '/community/notice' });
 
     const ids: number[] = [];
-    const nodeList = parentPage('.bbs_B_td_tr');
-
-    for (const node of nodeList) {
+    const nodes = $('.bbs_B_td_tr');
+    for (const node of nodes) {
         // 예를 들어 "goView(0, 719, 719);" 에서 719를 추출함
         const onClickText = node.attribs['onclick'];
         const splited = onClickText.split(',');
@@ -59,12 +58,12 @@ export async function getCurrentNoticeIds(): Promise<number[]> {
 }
 
 export async function getNotice(id: number) {
-    const childPage = await loadDocument("http://sw.anu.ac.kr/module/bbs/view.php", { 'rdno': id });
+    const $ = await loadDocument("http://sw.anu.ac.kr/module/bbs/view.php", { 'rdno': id });
 
-    const title = childPage('.bbs_title > .title').text().trim();
-    const author = childPage('#dpc_content > form > div.bbs_title > div.title_sub > dl > dd:nth-child(2)').text().trim();
-    const bodyText = childPage('#dpc_content > form > div.bbs_content').text().trim();
-    const createdAtText = childPage('#dpc_content > form > div.bbs_title > div.title_sub > dl > dd:nth-child(6)').text();
+    const title = $('.bbs_title > .title').text().trim();
+    const author = $('#dpc_content > form > div.bbs_title > div.title_sub > dl > dd:nth-child(2)').text().trim();
+    const bodyText = $('#dpc_content > form > div.bbs_content').text().trim();
+    const createdAtText = $('#dpc_content > form > div.bbs_title > div.title_sub > dl > dd:nth-child(6)').text();
     const createdAt = new Date(createdAtText);
 
     return new Notice(id, title, bodyText, author, createdAt);
--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,8 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (_req: Request, res: Response) => {
-    Promise.all([announceNewBusinesses(), announceNewNotices()])
+    const tasks = [announceNewBusinesses(), announceNewNotices()];
+    Promise.all(tasks)
         .then(() => {
             res.status(200).send('게시판 크롤링, 슬랙방 공지를 완료했습니다');
         }).catch((error: Error) => {
@@ -26,20 +27,21 @@ app.get('/', (_req: Request, res: Response) => {
 async function announceNewBusinesses(): Promise<void> {
     const ids = await board.getCurrentBusinessIds();
 
-    const taskList: Promise<void>[] = [];
+    const tasks: Promise<void>[] = [];
     for (const id of ids) {
         const task = announceBusiness(id);
-        taskList.push(task);
+        tasks.push(task);
     }
 
-    await Promise.all(taskList);
+    await Promise.all(tasks);
 }
 
 async function announceBusiness(id: number): Promise<void> {
     const business = await board.getBusiness(id);
     const found = await db.findBusiness(business._id);
     if (found === null) {
-        Promise.all([slack.notifyBusinessAdded(business), db.insertBusiness(business)]);
+        const tasks = [slack.notifyBusinessAdded(business), db.insertBusiness(business)];
+        Promise.all(tasks);
     } else {
         logger.info(`이미 알림한 지원사업입니다. ${business.title}`);
     }
@@ -48,20 +50,21 @@ async function announceBusiness(id: number): Promise<void> {
 async function announceNewNotices(): Promise<void> {
     const ids = await board.getCurrentNoticeIds();
 
-    const taskList: Promise<void>[] = [];
+    const tasks: Promise<void>[] = [];
     for (const id of ids) {
         const task = announceNotice(id);
-        taskList.push(task);
+        tasks.push(task);
     }
 
-    await Promise.all(taskList);
+    await Promise.all(tasks);
 }
 
 async function announceNotice(id: number): Promise<void> {
     const notice = await board.getNotice(id);
     const found = await db.findNotice(notice._id);
     if (found === null) {
-        Promise.all([slack.notifyNoticeAdded(notice), db.insertNotice(notice)]);
+        const tasks = [slack.notifyNoticeAdded(notice), db.insertNotice(notice)];
+        Promise.all(tasks);
     } else {
         logger.info(`이미 알림한 공지사항입니다. ${notice.title}`);
     }
