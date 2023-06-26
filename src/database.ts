be0ccc551ca6740f9d7749c97c6e36c132173cf2
Kim Hyeonseok <kim010322@gmail.com>
2023-06-26 16:39:18 +0900

    change connection string

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,11 @@
 import { MongoClient } from "mongodb";
-import { Notice } from "./entity/notice"
-import { Business } from "./entity/business"
+import { Notice } from "./entity/notice";
+import { Business } from "./entity/business";
 import { Webhook } from "./entity/webhook";
 
-const client = new MongoClient(process.env.CUSTOMCONNSTR_ANU_SW_BOT);
-const database = client.db('AnuSw');
+const client = new MongoClient(process.env.MONGODB_URL);
+const database = client.db("AnuSw");
 
-export const noticeRepository = database.collection<Notice>('notice');
-export const businessRepository = database.collection<Business>('business');
-export const webhookRepository = database.collection<Webhook>('webhook');
+export const noticeRepository = database.collection<Notice>("notice");
+export const businessRepository = database.collection<Business>("business");
+export const webhookRepository = database.collection<Webhook>("webhook");
